55b81ea8e593cccced5cd6df9e77185471114389
vrunoa <vruno.alassia@gmail.com>
2022-02-25 11:17:45 -0800

    add simple e2e test

---
+++ tests/e2e/simple.e2e.spec.js
@@ -0,0 +1,29 @@
+const remote = require('webdriverio').remote
+const { setupBindings } = require('./bindings')
+const { expect } = require('chai')
+
+describe('appium-plugin-template e2e', async () => {
+  let driver
+
+  it('should call plugin template', async () => {
+    const appiumCaps = {
+      hostname: 'localhost',
+      port: 4723,
+      protocol: 'http',
+      capabilities: {
+        platformName: 'Android',
+        'appium:automationName': 'UiAutomator2',
+        'appium:appPackage': 'com.google.android.googlequicksearchbox',
+        'appium:appActivity': 'com.google.android.apps.gsa.searchnow.SearchNowActivity',
+        'appium:avdName': 'Pixel_3a_API_30_x86'
+      }
+    }
+    driver = await remote(appiumCaps)
+    setupBindings(driver)
+    let pong = await driver.templatePing()
+    expect(pong).to.eq('pong')
+  })
+  after(async () => {
+    await driver.deleteSession()
+  })
+})
